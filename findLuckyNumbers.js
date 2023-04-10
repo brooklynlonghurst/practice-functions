be3a451403386934c104fc9a6f9e2f7ea9d7efc8
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-10 10:03:39 -0600

    monday exercise

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -3,11 +3,16 @@
 //i need to return an array 
 //with Math.random() for a unique number
 
-let listOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let answer = []
 
 function luckyNumber(num){
-    for(let i = 0; i < listOfNum.length; i++){
-        
+    let listOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    let answer = []
+    for(let i = 0; i < num; i++){
+        let x = Math.floor(Math.random() * listOfNum.length)
+        answer.push(listOfNum[x])
+        listOfNum.splice(x, 1)
     }
+    return answer
 }
+
+console.log(luckyNumber(7))
